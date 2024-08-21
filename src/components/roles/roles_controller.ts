7fe4787840ce0a032097c96373f0d7f414eb19b6
donkennie <ajeigbekehinde160@gmail.com>
2024-08-21 21:12:55 +0100

    add generic repository for reusable database functions

--- src/components/roles/roles_controller.ts
+++ src/components/roles/roles_controller.ts
@@ -1,23 +1,83 @@
-export class RoleController {
+import { Response, Request } from 'express';
+import { RolesService } from './roles_service';
+import { BaseController } from '../../utils/base_controller';
+import { Rights } from '../../utils/common';
 
-    public addHandler() {
-        // addHandler
+export class RoleController extends BaseController {
+
+    public async addHandler(req: Request, res: Response): Promise<void> {
+        const role = req.body;
+        const service = new RolesService();
+        const result = await service.create(role);
+        res.status(result.statusCode).json(result);
+        return;
     }
 
-    public getAllHandler() {
-        // getAllHandler
+    public async getAllHandler(req: Request, res: Response) { }
+
+    public async getOneHandler(req: Request, res: Response) { }
+
+    public async updateHandler(req: Request, res: Response) { }
+
+    public async deleteHandler(req: Request, res: Response) { }
+
+}
+
+export class RolesUtil {
+
+    /**
+     * Retrieves all possible permissions from the defined rights in the Rights object.
+     * @returns {string[]} An array of permissions
+     */
+    public static getAllPermissionsFromRights(): string[] {
+
+        // Initialize an empty array to collect values;
+        let permissions = [];
+
+        // Iterate through each section of the Rights object
+        for (const module in Rights) {
+
+            // Check if rights for ALL are defined for the current module
+            if (Rights[module]['ALL']) {
+                let sectionValues = Rights[module]['ALL'];
+                sectionValues = sectionValues.split(',');
+                permissions = [...permissions, ...sectionValues];
+            }
+        }
+        // Return the collected permissions
+        return permissions;
     }
 
-    public getDetailsHandler() {
-        // getDetailsHandler
+    public static async checkValidRoleIds(role_ids: string[]) {
+        const roleService = new RolesService();
+
+        // Query the database to check if all role_ids are valid
+        const roles = await roleService.findByIds(role_ids);
+
+        // Check if all role_ids are found in the database
+        return roles.data.length === role_ids.length;
     }
 
-    public async updateHandler() {
-        // updateHandler
-    }
+    // public static async getAllRightsFromRoles(role_ids: string[]): Promise<string[]> {
 
-    public async deleteHandler() {
-        // deleteHandler
-    }
+    //     // Create an instance of RolesService to interact with the roles
+    //     const roleService = new RolesService();
 
+    //     // Initialize an array to store the collected rights
+    //     let rights: string[] = [];
+
+    //     // Query the database to validate the provided role_ids
+    //     const queryData = await roleService.findByIds(role_ids);
+    //     const roles: Roles[] = queryData.data ? queryData.data : [];
+
+    //     // Extract rights from each role and add them to the rights array
+    //     roles.forEach((role) => {
+    //         const rightFromRole: string[] = role?.rights?.split(',');
+    //         rights = [...new Set(rights.concat(rightFromRole))];
+    //     });
+
+    //     // Return the accumulated rights
+    //     return rights;
+
+    // }
 }
